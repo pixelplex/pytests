3a27b4dd5221315f884ef05264c15cb3bb9af3cb
Kazak Aliaksei <a.kazak@pixelplex.io>
2019-07-02 14:24:48 +0300

    added main check

--- suites/DatabaseApi/GetContractFeePoolBalances.py
+++ suites/DatabaseApi/GetContractFeePoolBalances.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import lemoncheesecake.api as lcc
+from lemoncheesecake.matching import this_dict, check_that, has_length, check_that_entry, is_
 
 from common.base_test import BaseTest
 
@@ -47,19 +48,20 @@ class GetContractFeePoolBalances(BaseTest):
         contract_id = self.utils.get_contract_id(self, self.echo_acc0, self.contract, self.__database_api_identifier)
 
         lcc.set_step("Add fee pool to new contract")
-        broadcast_result = self.utils.perform_contract_fund_pool_operation(self, self.echo_acc0, contract_id,
-                                                                           value_to_poll,
-                                                                           self.__database_api_identifier,
-                                                                           log_broadcast=True)
-        contract_result = self.get_operation_results_ids(broadcast_result)
-        lcc.set_step("Get objects")
-        response_id = self.send_request(self.get_request("get_objects", [[contract_result]]),
-                                        self.__database_api_identifier, debug_mode=True)
-        response = self.get_response(response_id, log_response=True)
-        lcc.log_info("Call method")
+        self.utils.perform_contract_fund_pool_operation(self, self.echo_acc0, contract_id, value_to_poll,
+                                                        self.__database_api_identifier)
+        lcc.log_info("Fee pull added to '{}' contract successfully".format(contract_id))
 
-        lcc.set_step("Get a contract's pool balance in default asset")
+        lcc.set_step("Get a contract's fee pool balance")
         response_id = self.send_request(self.get_request("get_contract_pool_balance", [contract_id]),
-                                        self.__database_api_identifier, debug_mode=True)
-        response = self.get_response(response_id, log_response=True)
+                                        self.__database_api_identifier)
+        result = self.get_response(response_id)["result"]
         lcc.log_info("Call method 'get_contract_pool_balance' with param: '{}'".format(contract_id))
+
+        lcc.set_step("Check simple work of method 'get_contract'")
+        with this_dict(result):
+            if check_that("contract pool balance", result, has_length(2)):
+                check_that_entry("amount", is_(value_to_poll))
+                check_that_entry("asset_id", is_(self.echo_asset))
+
+
