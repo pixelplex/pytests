e01f08a65b42d3c5bab16fd22c241769a0cfd19f
Kazak Aliaksei <a.kazak@pixelplex.io>
2019-07-08 14:20:13 +0300

    Resolve ECHO-814: Bag fixed. Test added.

--- suites/DatabaseApi/SetSubscribeCallback.py
+++ suites/DatabaseApi/SetSubscribeCallback.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import lemoncheesecake.api as lcc
 from lemoncheesecake.matching import check_that, is_none, is_list, is_, has_entry, check_that_in, not_equal_to, \
-    require_that
+    require_that, equal_to
 
 from common.base_test import BaseTest
 
@@ -87,6 +87,9 @@ class PositiveTesting(BaseTest):
     def __init__(self):
         super().__init__()
         self.__database_api_identifier = None
+        self.__registration_api_identifier = None
+        self.contract_piggy = self.get_byte_code("piggy", "code")
+        self.break_piggy = self.get_byte_code("piggy", "breakPiggy")
 
     def set_subscribe_callback(self, callback, notify_remove_create=False):
         params = [callback, notify_remove_create]
@@ -99,11 +102,22 @@ class PositiveTesting(BaseTest):
 
     def setup_suite(self):
         super().setup_suite()
+        self._connect_to_echopy_lib()
         lcc.set_step("Setup for {}".format(self.__class__.__name__))
         self.__database_api_identifier = self.get_identifier("database")
-        lcc.log_info("Database API identifier is '{}'".format(self.__database_api_identifier))
+        self.__registration_api_identifier = self.get_identifier("registration")
+        lcc.log_info(
+            "API identifiers are: database='{}', registration='{}'".format(self.__database_api_identifier,
+                                                                           self.__registration_api_identifier))
+        self.echo_acc0 = self.get_account_id(self.echo_acc0, self.__database_api_identifier,
+                                             self.__registration_api_identifier)
+        lcc.log_info("Echo account is '{}'".format(self.echo_acc0))
         self.utils.cancel_all_subscriptions(self, self.__database_api_identifier)
         lcc.log_info("Canceled all subscriptions successfully")
+
+    def teardown_suite(self):
+        self._disconnect_to_echopy_lib()
+        super().teardown_suite()
 
     @lcc.prop("type", "method")
     @lcc.test("Check notices of dynamic object '2.1.0'")
@@ -149,3 +163,44 @@ class PositiveTesting(BaseTest):
             "recent_slots_filled", not_equal_to(response["recent_slots_filled"]),
             "last_irreversible_block_num", not_equal_to(response["last_irreversible_block_num"]),
         )
+
+    @lcc.prop("type", "method")
+    @lcc.test("Check notices of created contract")
+    @lcc.depends_on("DatabaseApi.SetSubscribeCallback.SetSubscribeCallback.method_main_check")
+    def check_contract_notices(self, get_random_integer):
+        lcc.set_step("Set subscribe callback")
+        subscription_callback_id = get_random_integer
+        self.set_subscribe_callback(subscription_callback_id, notify_remove_create=True)
+
+        lcc.set_step("Create 'piggy' contract in ECHO network")
+        contract_id = self.utils.get_contract_id(self, self.echo_acc0, self.contract_piggy,
+                                                 self.__database_api_identifier, need_broadcast_result=True)
+
+        lcc.set_step("Get notice about created contract")
+        notice_about_created_contract = self.get_notice(subscription_callback_id,
+                                                        object_id=self.get_implementation_object_type(
+                                                            self.echo.config.implementation_object_types.TRANSACTION))
+
+        lcc.set_step("Check notice about created contract")
+        del contract_id.get("broadcast_result")["trx"]["operation_results"]
+        check_that("'received notice'", contract_id.get("broadcast_result").get("trx"),
+                   equal_to(notice_about_created_contract.get("trx")), quiet=True)
+
+        lcc.set_step("Destroy the contract. Call 'breakPiggy' method")
+        operation = self.echo_ops.get_call_contract_operation(echo=self.echo, registrar=self.echo_acc0,
+                                                              bytecode=self.break_piggy,
+                                                              callee=contract_id.get("contract_id"))
+        collected_operation = self.collect_operations(operation, self.__database_api_identifier)
+        broadcast_result = self.echo_ops.broadcast(echo=self.echo, list_operations=collected_operation,
+                                                   log_broadcast=False)
+        lcc.log_info("Contract method 'breakPiggy' is called successfully")
+
+        lcc.set_step("Get notice about call contract")
+        notice_about_call_contract = self.get_notice(subscription_callback_id,
+                                                     object_id=self.get_implementation_object_type(
+                                                         self.echo.config.implementation_object_types.TRANSACTION))
+
+        lcc.set_step("Check notice about call contract")
+        del broadcast_result.get("trx")["operation_results"]
+        check_that("'received notice'", broadcast_result.get("trx"),
+                   equal_to(notice_about_call_contract.get("trx")), quiet=True)
