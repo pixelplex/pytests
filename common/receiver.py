70d33f1f553b23897d91bac7b8d763cf20e735ff
y.sidoruk <y.sidoruk@pixelplex.io>
2019-07-16 20:46:13 +0300

    fix get_notice

--- common/receiver.py
+++ common/receiver.py
@@ -116,18 +116,13 @@ class Receiver(object):
             return notice_params
         if isinstance(notice_params, list):
             for notice_param in notice_params:
-                if (notice_params.get("address")) and (self.validator.is_hex(notice_params.get("log")[0])):
+                if (notice_param["address"]) and (self.validator.is_hex(notice_param["log"][0])):
                     if print_log:
                         lcc.log_info(
                             "Received notice about new contract logs:\n{}".format(json.dumps(response, indent=4)))
                     return notice_params
-                if (notice_params.get("block_num")) and (self.validator.is_hex(notice_params.get("tx_id"))):
-                    if print_log:
-                        lcc.log_info(
-                            "Received notice about successful creation of new account:\n{}".format(
-                                json.dumps(response, indent=4)))
-                    return notice_params
         if (notice_params.get("address")) and (self.validator.is_hex(notice_params.get("log")[0])):
+            lcc.log_error("Do we use this method?")
             if print_log:
                 lcc.log_info(
                     "Received notice about new contract logs:\n{}".format(json.dumps(response, indent=4)))
