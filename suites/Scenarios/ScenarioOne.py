000c01ccd4a7c1c83d5db801f14be7d61791f8f8
d.bulka <d.bulka@pixelplex.io>
2019-07-18 17:53:53 +0300

    added transfer from account1 to account2 with testing

--- suites/Scenarios/ScenarioOne.py
+++ suites/Scenarios/ScenarioOne.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import lemoncheesecake.api as lcc
 from lemoncheesecake.matching import check_that, equal_to
-from fixtures.base_fixtures import get_random_integer_up_to_hundred
+from fixtures.base_fixtures import get_random_integer_up_to_hundred, get_random_integer
 from common.base_test import BaseTest
 
 SUITE = {
@@ -46,17 +46,20 @@ class ScenarioOne(BaseTest):
     def main(self, get_random_valid_account_name):
         account_count = 2
         account_names = [get_random_valid_account_name + str(i) for i in range(account_count)]
+        account_ids = []
+        private_keys = []
         for account_name in account_names:
-            public_key = self.generate_keys()[1]
-
-            lcc.set_step("Create account")
+            keys = self.generate_keys()
+            public_key = keys[1]
+            private_key = keys[0]
+            lcc.set_step("Create account '{}'".format(account_name))
             create_account_operation = self.echo_ops.get_account_create_operation(self.echo, account_name, public_key,
                                                                                   public_key,
                                                                                   registrar=self.echo_acc0,
                                                                                   signer=self.echo_acc0)
-
             collected_operation = self.collect_operations(create_account_operation, self.__database_api_identifier)
-            broadcast_result = self.echo_ops.broadcast(echo=self.echo, list_operations=collected_operation)
+            broadcast_result = self.echo_ops.broadcast(echo=self.echo, list_operations=collected_operation,
+                                                       log_broadcast=False)
             if not self.is_operation_completed(broadcast_result, expected_static_variant=1):
                 raise Exception("Account is not created")
             operation_result = self.get_operation_results_ids(broadcast_result)
@@ -71,21 +74,96 @@ class ScenarioOne(BaseTest):
                 lcc.log_error("Account '{}' is wrong".format(account_id_from_broadcast_result))
             else:
                 lcc.log_info("Account '{}' is correct".format(account_id_from_broadcast_result))
+            account_ids.append(account_id)
+            private_keys.append(private_key)
 
-            lcc.set_step("Transfer from echo_acc0 to 1st account")
-            amount = get_random_integer_up_to_hundred()
-            lcc.log_debug(str(amount))
-            transfer_operation = self.echo_ops.get_transfer_operation(echo=self.echo,
-                                                                      from_account_id=self.echo_acc0,
-                                                                      to_account_id=account_id,
-                                                                      amount=amount, debug_mode=True)
-            collected_operation = self.collect_operations(transfer_operation, self.__database_api_identifier,
-                                                          debug_mode=True)
-            broadcast_result = self.echo_ops.broadcast(echo=self.echo, list_operations=collected_operation,
-                                                       debug_mode=True)
-            if not self.is_operation_completed(broadcast_result, expected_static_variant=0):
-                raise Exception("Account is not created")
-            else:
-                lcc.log_info("Transfer was done: from {} to {}, amount = {}".format(self.echo_acc0, account_id, amount))
+        lcc.set_step("Transfer from echo_acc0 to 1st account")
+        amount = get_random_integer()
+        asset_id = "1.3.0"
+        account_id = account_ids[0]
+        response_account1_amount_id = self.send_request(
+            self.get_request("get_account_balances", [account_id, [asset_id]]),
+            self.__database_api_identifier)
+        account1_amount_before_transfer = \
+        self.get_response(response_account1_amount_id)["result"][0]["amount"]
+        response_echo_acc0_amount_id = self.send_request(
+            self.get_request("get_account_balances", [self.echo_acc0, [asset_id]]),
+            self.__database_api_identifier)
+        echo_acc0_amount_before_transfer = \
+            self.get_response(response_echo_acc0_amount_id)["result"][0]["amount"]
+        transfer_operation = self.echo_ops.get_transfer_operation(echo=self.echo,
+                                                                  from_account_id=self.echo_acc0,
+                                                                  to_account_id=account_id,
+                                                                  amount=amount)
+        fee_amount = self.get_required_fee(transfer_operation, self.__database_api_identifier, debug_mode=True)[0]["amount"]
+        collected_operation = self.collect_operations(transfer_operation, self.__database_api_identifier)
+        broadcast_result = self.echo_ops.broadcast(echo=self.echo, list_operations=collected_operation, log_broadcast=False)
+        if not self.is_operation_completed(broadcast_result, expected_static_variant=0):
+            raise Exception("Account is not created")
+        else:
+            lcc.log_info("Transfer was done: from {} to {}, amount = {}".format(self.echo_acc0, account_id, amount))
+        lcc.log_debug(str(fee_amount))
+        response_account1_amount_id = self.send_request(
+            self.get_request("get_account_balances", [account_id, [asset_id]]),
+            self.__database_api_identifier)
+        account1_amount_after_transfer = self.get_response(response_account1_amount_id)["result"][0][
+            "amount"]
+        response_echo_acc0_amount_id = self.send_request(
+            self.get_request("get_account_balances", [self.echo_acc0, [asset_id]]),
+            self.__database_api_identifier)
+        echo_acc0_amount_after_transfer = \
+            self.get_response(response_echo_acc0_amount_id)["result"][0]["amount"]
+        if (int(account1_amount_after_transfer) - int(account1_amount_before_transfer)) != amount:
+            lcc.log_error("Transfer is wrong, incorrect amount of account1")
+        elif (int(echo_acc0_amount_before_transfer) - int(echo_acc0_amount_after_transfer) - fee_amount) != amount:
+            lcc.log_error("Transfer is wrong, incorrect amount of acc0")
+        else:
+            lcc.log_info("Transfer is correct. Account1 got amount: {}, fee is '{}' ".format(amount, fee_amount))
+        account_ids.append(account_id)
 
+        lcc.set_step("Transfer from 2st account to 2nd account")
+        amount = amount-fee_amount-1
 
+        account1_id = account_ids[0]
+        account2_id = account_ids[1]
+
+        response_account1_amount_id = self.send_request(
+            self.get_request("get_account_balances", [account1_id, [asset_id]]),
+            self.__database_api_identifier)
+        account1_amount_before_transfer = \
+            self.get_response(response_account1_amount_id)["result"][0]["amount"]
+        response_account2_amount_id = self.send_request(
+            self.get_request("get_account_balances", [account2_id, [asset_id]]),
+            self.__database_api_identifier)
+        account2_amount_before_transfer = \
+            self.get_response(response_account2_amount_id)["result"][0]["amount"]
+        transfer_operation = self.echo_ops.get_transfer_operation(echo=self.echo,
+                                                                  from_account_id=account1_id,
+                                                                  to_account_id=account2_id,
+                                                                  amount=amount, debug_mode=True, signer=private_keys[0])
+
+        collected_operation = self.collect_operations(transfer_operation, self.__database_api_identifier, debug_mode=True)
+        broadcast_result = self.echo_ops.broadcast(echo=self.echo, list_operations=collected_operation, debug_mode=True)
+        if not self.is_operation_completed(broadcast_result, expected_static_variant=0):
+            raise Exception("Account is not created")
+        else:
+            lcc.log_info("Transfer was done: from {} to {}, amount = {}".format(self.echo_acc0, account_id, amount))
+        fee_amount = self.get_required_fee(transfer_operation, self.__database_api_identifier, debug_mode=True)[0][
+            "amount"]
+        response_account1_amount_id = self.send_request(
+            self.get_request("get_account_balances", [account1_id, [asset_id]]),
+            self.__database_api_identifier)
+        account1_amount_after_transfer = \
+            self.get_response(response_account1_amount_id)["result"][0]["amount"]
+        response_account2_amount_id = self.send_request(
+            self.get_request("get_account_balances", [account2_id, [asset_id]]),
+            self.__database_api_identifier)
+        account2_amount_after_transfer = \
+            self.get_response(response_account2_amount_id)["result"][0]["amount"]
+        if (int(account2_amount_after_transfer) - int(account2_amount_before_transfer)) != amount:
+            lcc.log_error("Transfer is wrong, incorrect amount of account1")
+        elif (int(account1_amount_before_transfer) - int(account1_amount_after_transfer) - fee_amount) != amount:
+            lcc.log_error("Transfer is wrong, incorrect amount of acc0")
+        else:
+            lcc.log_info("Transfer is correct. Account1 got amount: {}, fee is '{}' ".format(amount, fee_amount))
+        account_ids.append(account_id)
