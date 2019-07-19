7a88ead1698cd031c35a016411b0abcb56e79189
Kazak Aliaksei <a.kazak@pixelplex.io>
2019-07-19 14:28:11 +0300

    fix pre_run

--- pre_run_scripts/pre_deploy.py
+++ pre_run_scripts/pre_deploy.py
@@ -95,9 +95,10 @@ def distribute_balance_between_committee_addresses(base_test):
         transaction = base_test.eth_trx.get_transfer_transaction(web3=base_test.web3,
                                                                  to=INITIAL_ACCOUNTS_ETH_ADDRESSES[i],
                                                                  value=balance_to_transfer, debug_mode=True)
-        a = base_test.eth_trx.broadcast(web3=base_test.web3, transaction=transaction, log_transaction=False)
-        print("!!!HERE!!! -> " + str(a))
-        if a is None:
+        broadcast_result = base_test.eth_trx.broadcast(web3=base_test.web3, transaction=transaction,
+                                                       log_transaction=False)
+        print("!!!HERE!!! -> " + str(broadcast_result))
+        if broadcast_result is None:
             return False
     return True
 
