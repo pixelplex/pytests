58645a6d2b3e9f8335892fb8721c7dfa7085c46e
Kazak Aliaksei <a.kazak@pixelplex.io>
2019-07-25 09:44:54 +0300

    fixed changing active committee member

--- suites/Scenarios/ChangeActiveCommitteeMember.py
+++ suites/Scenarios/ChangeActiveCommitteeMember.py
@@ -52,7 +52,7 @@ class ChangeActiveCommitteeMember(BaseTest):
 
     def setup_suite(self):
         super().setup_suite()
-        self._connect_to_ganache_ethereum()
+        self._connect_to_ethereum()
         self._connect_to_echopy_lib()
         lcc.set_step("Setup for {}".format(self.__class__.__name__))
         self.__database_api_identifier = self.get_identifier("database")
