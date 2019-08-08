0f9c22ef3264595c1b3aa02661f78d2fabe45cdd
d.bulka <d.bulka@pixelplex.io>
2019-08-08 10:28:33 +0300

    added new branch ECHO-1063

---
+++ suites/DatabaseApi/GetERC20AccountDeposits.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import lemoncheesecake.api as lcc
+
+from common.base_test import BaseTest
+
+SUITE = {
+    "description": "Method 'get_erc20_account_deposits'"
+}
+
+
+@lcc.prop("suite_run_option_1", "main")
+@lcc.prop("suite_run_option_2", "positive")
+@lcc.prop("suite_run_option_3", "negative")
+@lcc.tags("database_api", "get_erc20_account_deposits", "sidechain")
+@lcc.suite("Check work of method 'get_erc20_account_deposits'", rank=1)
+class GetERC20AccountDeposits(BaseTest):
+
+    def __init__(self):
+        super().__init__()
+        self.__database_api_identifier = None
+        self.__registration_api_identifier = None
+        self.echo_acc0 = None
+
+    def setup_suite(self):
+        super().setup_suite()
+        self._connect_to_ethereum()
+        self._connect_to_echopy_lib()
+        lcc.set_step("Setup for {}".format(self.__class__.__name__))
+        self.__database_api_identifier = self.get_identifier("database")
+        self.__registration_api_identifier = self.get_identifier("registration")
+        lcc.log_info(
+            "API identifiers are: database='{}', registration='{}'".format(self.__database_api_identifier,
+                                                                           self.__registration_api_identifier))
+        self.echo_acc0 = self.get_account_id(self.accounts[0], self.__database_api_identifier,
+                                             self.__registration_api_identifier)
+        lcc.log_info("Echo account is '{}'".format(self.echo_acc0))
+
+    def teardown_suite(self):
+        self._disconnect_to_echopy_lib()
+        super().teardown_suite()
+
+    @lcc.prop("type", "method")
+    @lcc.test("Simple work of method 'get_erc20_account_deposits'")
+    def method_main_check(self):
+        pass
